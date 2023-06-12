97485265086c1db23580435c349a0844f2441232
Sarthak Pant <67951999+sarthakpant772@users.noreply.github.com>
2023-06-12 14:22:19 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -4,38 +4,59 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-
+        
+        int MOD = 1e9+7;
+        int mod = 1e9+7;
 	public:
 	
-	int help(int arr[] , int n, int sum , int i , int x , vector<vector<int>> &dp){
-	    int mod = 1e9+7;
-	    if(i>=n){
-	        if(sum==x){
+	int recur(int arr[] , int n , int sum){     
+	   if(n==0){
+            
+            if(sum==0){
+                return 1;
+            }
+            return 0;
+	        
+	   }
+	        
+	   if(arr[n-1]>sum){
+	       return recur(arr,n-1,sum)%MOD;
+	   }
+	   else {
+	       return (recur(arr,n-1,sum-arr[n-1])%MOD+recur(arr,n-1,sum)%MOD)%MOD;
+	   }
+	}
+	
+	int memo(int arr[] , int n , int sum , vector<vector<int>> &dp){
+	    
+	    if(n==0){
+	        if(sum==0){
 	            return 1;
 	        }
 	        return 0;
 	    }
-	    int l =0 , r=0;
 	    
-	    if(dp[i][x]!=-1){
-	        return dp[i][x];
+	    if(dp[n][sum]!=-1){
+	        return dp[n][sum];
 	    }
 	    
-	    if(x+arr[i]<=sum){
-	        l=help(arr,n,sum,i+1,x+arr[i],dp)%mod;
-	    }
-	    r=help(arr,n,sum,i+1,x,dp)%mod;
-	    return dp[i][x]=  (l+r)%mod;
+	   if(arr[n-1]>sum){
+	       return dp[n][sum]=memo(arr,n-1,sum,dp)%MOD;
+	   }
+	   else {
+	       return dp[n][sum]=(memo(arr,n-1,sum-arr[n-1],dp)%MOD+memo(arr,n-1,sum,dp)%MOD)%MOD;
+	   }
+	   
+	   
 	    
 	}
 	
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
+        vector<vector<int>> dp(n+1 , vector<int>(sum+1,-1));
+        return memo(arr,n,sum,dp);
         
-        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
-      
-        return help(arr,n,sum,0,0,dp);
 	}
 	  
 };
