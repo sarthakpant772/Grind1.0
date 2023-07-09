c5684f9743a1f10d3c73e1d3670e17147e5fb488
Sarthak Pant <67951999+sarthakpant772@users.noreply.github.com>
2023-07-09 18:14:10 +0530

    Added solution - LeetHub

---
+++ Find pairs with given sum in doubly linked list - GFG/find-pairs-with-given-sum-in-doubly-linked-list.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/* Doubly linked list node class
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+*/
+
+class Solution
+{
+public:
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target)
+    {
+        // code 
+        
+        vector<pair<int,int>> vp;
+        
+        map<int,int> mp;
+        
+        if(!head)
+            return vp;
+            
+        while(head){
+            
+            
+            if(mp.count(target-head->data)){
+                vp.push_back({min(head->data, target-head->data) ,max(head->data, target-head->data)});
+            }
+            
+            mp[head->data]++;
+            head= head->next;
+        }
+        
+        sort(vp.begin(),vp.end());
+        
+        return vp;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, target;
+        cin >> target >> n;
+        int a;
+        cin >> a;
+        Node *head = new Node(a);
+        Node *tail = head;
+        for (int i = 0; i < n - 1; i++)
+        {
+            cin >> a;
+            tail->next = new Node(a);
+            tail->next->prev = tail;
+            tail = tail->next;
+        }
+        Solution ob;
+        auto ans = ob.findPairsWithGivenSum(head, target);
+        if (ans.size() == 0)
+            cout << "-1";
+        else
+        {
+            for (int i = 0; i < ans.size(); i++)
+            {
+                cout << "(" << ans[i].first << "," << ans[i].second << ")"
+                     << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
