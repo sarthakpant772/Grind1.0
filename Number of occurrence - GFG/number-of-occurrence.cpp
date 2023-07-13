fda9e621aeadcf413a5fd47bae2b235dea3ad9be
Sarthak Pant <67951999+sarthakpant772@users.noreply.github.com>
2023-07-13 18:26:50 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,49 +10,42 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-	int count(int arr[], int n, int x) {
+	int count(int nums[], int n, int x) {
 	    // code here
-	    int start =-1 , end=-1;
-	    
-	    
-	    int l =0 , r=n-1;
+	    int l = 0 , r = n-1;
+	    int first=-1 , last=-1;
 	    
 	    while(l<=r){
 	        int mid = (l+r)/2;
 	        
-	        if(arr[mid]>x){
-	            r= mid-1;
-	        }
-	        else if(arr[mid]<x){
-	            l=mid+1;
+	        if(nums[mid]>=x){
+	            r=  mid-1;
+	            first = mid;
 	        }
 	        else{
-	            start = mid;
-	            r = mid-1;
+	            l = mid+1;
 	        }
+	        
 	    }
 	    
-	    if(start==-1)
-	        return 0;
+	    if(first!=-1){
+	        l =0 , r = n-1;
+	        while(l<=r){
+	            int mid = (l+r)/2;
+	            
+	            if(nums[mid]<=x){
+	                l =  mid+1;
+	                last = mid;
+	            }
+	            else{
+	                r = mid-1;
+	            }
+	        }
 	        
-	   l=0  , r=n-1;
-	   
-	    while(l<=r){
-                int mid = (l+r)/2;
-    	        
-    	        if(arr[mid]>x){
-    	            r= mid-1;
-    	        }
-    	        else if(arr[mid]<x){
-    	            l=mid+1;
-    	        }
-    	        else{
-    	            end = mid;
-    	            l = mid+1;
-    	        }
-	    } 
-	   // cout<<start<<" "<<end<<endl;
-	    return end-start+1;
+	    }
+	    else return 0;
+	    
+	    return (last - first)+1;
 	}
 };
 
