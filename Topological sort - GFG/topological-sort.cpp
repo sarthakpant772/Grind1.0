06ac21bb509e78074367bc60555bc7f9cc225c32
Sarthak Pant <67951999+sarthakpant772@users.noreply.github.com>
2023-08-15 12:21:18 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -19,19 +19,54 @@ class Solution
 	   
 	   ans.push_back(i);
 	}
+// 	KHANS algo
+
+    void khans(vector<int> adj[] , int V , vector<int> &ans){
+        vector<int> indegree(V , 0);
+        
+        for(int i=0;i<V;++i){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        
+        queue<int> q;
+        
+        for(int i=0;i<V;++i){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int x = q.front();
+            q.pop();
+            ans.push_back(x);
+            for(auto it:adj[x]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        
+    }
+    
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
 	    vector<int> vis(V,-1);
 	    vector<int> ans;
         
-        for(int i=0;i<V;++i){
-            if(vis[i]==-1){
-                dfs(adj , i , ans , vis);
-            }
-        }
+        // for(int i=0;i<V;++i){
+        //     if(vis[i]==-1){
+        //         dfs(adj , i , ans , vis);
+        //     }
+        // }
+        
+        khans(adj , V , ans);
         // cout<<ans.size();
-        reverse(ans.begin() , ans.end());
+        // reverse(ans.begin() , ans.end());
         return ans;
 	}
 };
